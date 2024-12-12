81972762c596127452a1778c8d6199c0696ee406
RyanChehab <ryanchehab20@gmail.com>
2024-12-12 16:10:00 +0200

    php <=> created login function

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class JWTAuthController extends Controller{
-    
+
+    public function login(Request $request){
+        //Validating the request
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+    }
 }
