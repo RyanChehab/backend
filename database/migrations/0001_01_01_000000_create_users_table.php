b61076c5a14061598861992510478a6da68e2661
RyanChehab <ryanchehab20@gmail.com>
2024-12-12 16:35:21 +0200

    php <=> edited users schema

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->enum('user_type', ['admin', 'writer', 'reader'])->default('reader');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
 
