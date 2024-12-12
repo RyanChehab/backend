26518941e672c2b74fb313860af49742a5fb69cf
RyanChehab <ryanchehab20@gmail.com>
2024-12-12 17:56:29 +0200

    php <=>routed signup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
-use App\Http\Controllers\JWTAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\JWTMiddleware;
+use App\Http\Controllers\JWTAuthController;
+
+Route::post('/signup',[JWTAuthController::class, 'signup']);
