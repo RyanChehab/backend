3f432e0314d538ad66859543923cf2efb271de49
RyanChehab <ryanchehab20@gmail.com>
2024-12-12 17:51:58 +0200

    php <=> used JWTAuthController

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
+use App\Http\Controllers\JWTAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
