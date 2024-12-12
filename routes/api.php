27484d2d619dca100d65a1907c9d1aa6b292846d
RyanChehab <ryanchehab20@gmail.com>
2024-12-12 23:55:26 +0200

    php <=> routed delete user and block user methods

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,7 @@ Route::post('/login',[JWTAuthController::class, 'login']);
 
 Route::middleware(JWTMiddleware::class)->group(function(){
     Route::post('/logout',[JWTAuthController::class, 'logout']);
+    Route::post('delete_user',[JWTAuthController::class, 'delete_user']);
 });
+
+Route::post('/block_user',[JWTAuthController::class, 'block_user']);
