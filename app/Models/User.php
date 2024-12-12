2bb50c2f3cd872ef366f3c15e38adf7caacc89fb
RyanChehab <ryanchehab20@gmail.com>
2024-12-12 21:43:42 +0200

    php <=> added user_type key

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
-use Illuminate\Notifications\Notifiable;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject{
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -14,19 +15,20 @@ class User extends Authenticatable implements JWTSubject{
     /**
      * The attributes that are mass assignable.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
         'username',
         'email',
         'password',
+        'user_type',
     ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var list<string>
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
