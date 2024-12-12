ef37a398992271c5a6b8aa1665589517c276ad7e
RyanChehab <ryanchehab20@gmail.com>
2024-12-12 23:54:37 +0200

    php <=> used JWTAuth and JWTExceptions

--- app/Http/Middleware/JWTMiddleware.php
+++ app/Http/Middleware/JWTMiddleware.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class JWTMiddleware
 {
@@ -13,8 +15,7 @@ class JWTMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
-    {
+    public function handle(Request $request, Closure $next){
         try{
             
             $user = JWTAuth::parseToken()->authenticate();
