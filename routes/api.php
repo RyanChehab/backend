1471aeca4589bc48313a9e7bd543c39fcde908b3
RyanChehab <ryanchehab20@gmail.com>
2024-12-13 03:15:58 +0200

    php <=> protected block user api with admin middleware

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\JWTMiddleware;
+use App\Http\Middleware\AdminMiddleware;
 use App\Http\Controllers\JWTAuthController;
 
 Route::post('/signup',[JWTAuthController::class, 'signup']);
 
 Route::post('/login',[JWTAuthController::class, 'login']);
 
+// Blocking user 
+Route::middleware(AdminMiddleware::class)->group(function(){
+    Route::post('/block_user',[JWTAuthController::class, 'block_user']);
+});
+
+// Deleting user and logingout
 Route::middleware(JWTMiddleware::class)->group(function(){
     Route::post('/logout',[JWTAuthController::class, 'logout']);
     Route::post('delete_user',[JWTAuthController::class, 'delete_user']);
 });
-
-Route::post('/block_user',[JWTAuthController::class, 'block_user']);
