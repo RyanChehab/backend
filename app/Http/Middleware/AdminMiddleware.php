7b1096736f729b0b5d18485930e057ecc2bd5eef
RyanChehab <ryanchehab20@gmail.com>
2024-12-13 03:09:05 +0200

    php <=> added try catch blocks in admin middleware

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -8,15 +8,19 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use Symfony\Component\HttpFoundation\Response;
 
-class AdminMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
+class AdminMiddleware{
+    
     public function handle(Request $request, Closure $next): Response
     {
+        try{
+            $user = JWTAuth::parseToken()->authenticate();
+
+            if($user->user_type !== 'admin'){
+                return response()->json([
+                    'message'=>'Access denied. Admins only'
+                ],403);
+            }
+        }catch
         return $next($request);
     }
 }
