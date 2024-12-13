5c201c40b027c74bb22af48865649c6322cba920
RyanChehab <ryanchehab20@gmail.com>
2024-12-13 03:05:49 +0200

    php ,=> used JWTException and JWTAuth

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
