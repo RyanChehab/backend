5a8d077bef255992ebddfba2d8bafbe9a89c6350
RyanChehab <ryanchehab20@gmail.com>
2024-12-20 04:14:11 +0200

    php <=> routed resetpassword api

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\JWTMiddleware;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Controllers\JWTAuthController;
-
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ResetPasswordMail;
 Route::post('/signup',[JWTAuthController::class, 'signup']);
 
 Route::post('/login',[JWTAuthController::class, 'login']);
+
+Route::post('/reset', [JWTAuthController::class, 'resetPassword']);
 
 // Blocking user 
 Route::middleware(AdminMiddleware::class)->group(function(){
@@ -20,3 +23,8 @@ Route::middleware(JWTMiddleware::class)->group(function(){
     Route::post('/logout',[JWTAuthController::class, 'logout']);
     Route::post('delete_user',[JWTAuthController::class, 'delete_user']);
 });
+
+// Route::get('/test-email', function () {
+//     Mail::to('test@example.com')->send(new \App\Mail\ResetPasswordMail('http://example.com/reset-password?token=12345'));
+//     return 'Email sent!';
+// });
