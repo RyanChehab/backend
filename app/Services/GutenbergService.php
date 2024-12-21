ee2647d8d34a3c419571f4203962f92eb71e433f
RyanChehab <ryanchehab20@gmail.com>
2024-12-21 21:19:26 +0200

    php <=> stored response

--- app/Services/GutenbergService.php
+++ app/Services/GutenbergService.php
@@ -8,8 +8,16 @@ Class GutenbergService{
     protected $baseUrl =  'https://gutendex.com/books/';
 
     public function fetchBooks(int $limit = 16):array{
+
     $response = Http::get($this->baseUrl, [
         'sort' => 'download_count'
     ]);
+
+    if ($response->failed()) {
+        throw new Exception('Failed to fetch data from the Gutenberg API.');
+    }
+
+    $books = $response->json('results');
+
     }
 }
