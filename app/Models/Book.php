bd20692d001287f205531b611f7fb0515ed17ab1
RyanChehab <ryanchehab20@gmail.com>
2024-12-21 18:21:30 +0200

    php <=> added fillable keys in model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,4 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 useIlluminate\Database\Eloquent\Factories\HasFactory;
 class Book extends Model{
     use HasFactory;
+
+    protected $fillable = [
+        'gutenberg_id',
+        'title',
+        'author',
+        'category',
+        'url_text',
+        'url_img',
+        'downloads',
+        'featured',
+        'used',
+    ];
 }
