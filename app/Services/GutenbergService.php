a737089d9c2fd1321075adf16bb1e2fb3c111e0a
RyanChehab <ryanchehab20@gmail.com>
2024-12-21 21:10:18 +0200

    php <=> created public function fetchBooks

--- app/Services/GutenbergService.php
+++ app/Services/GutenbergService.php
@@ -6,5 +6,10 @@ use Illuminate\Support\Facades\Http;
 
 Class GutenbergService{
     protected $baseUrl =  'https://gutendex.com/books/';
-    
+
+    public function fetchBooks(int $limit = 16):array{
+    $response = Http::get($this->baseUrl, [
+        'sort' => 'download_count'
+    ]);
+    }
 }
