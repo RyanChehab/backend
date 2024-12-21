69f78a39663e3e43c2e475373c1f37fe6a65d98e
RyanChehab <ryanchehab20@gmail.com>
2024-12-21 21:39:06 +0200

    php <=> created public function populate

--- app/Http/Controllers/PopulateBooksController.php
+++ app/Http/Controllers/PopulateBooksController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\books;
 use Illuminate\Http\Request;
+use App\Services\GutenbergService;
 
-class PopulateBooksController extends Controller
-{
-    //
+class PopulateBooksController extends Controller{
+    
+    protected $gutenbergService;
+
+    public function __construct(GutenbergService $gutenbergService){
+
+        $this->gutenbergService = $gutenbergService;
+    
+    }
+
+    public function populate(){
+        try{
+            $books = $this->$gutenbergService->fetchBooks();
+            
+        }
+    }
 }
