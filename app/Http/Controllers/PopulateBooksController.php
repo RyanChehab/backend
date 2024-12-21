5e2167b5a9edf62feca63a7708607546d7c5e4b2
RyanChehab <ryanchehab20@gmail.com>
2024-12-21 22:44:53 +0200

    php <=> created determinCategory private function

--- app/Http/Controllers/PopulateBooksController.php
+++ app/Http/Controllers/PopulateBooksController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\books;
+use App\Models\Book;
 use Illuminate\Http\Request;
 use App\Services\GutenbergService;
 
@@ -19,7 +19,18 @@ class PopulateBooksController extends Controller{
     public function populate(){
         try{
             $books = $this->$gutenbergService->fetchBooks();
-            
+
+            foreach ($books as $book){
+                if(!in_array('en',$book['languages']?? [])){
+                    continue;
+                }
+
+                $category = $this->determinCategory($book[bookshelves]);
+
+                Book::updateOrCreate();
+            }
+        }catch(){
+
         }
     }
 }
