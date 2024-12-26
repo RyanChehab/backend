c7e57958610f6e34b9492a3e3cf032bd112b2a79
RyanChehab <ryanchehab20@gmail.com>
2024-12-26 02:37:29 +0200

    migrations <=> added nullable attribute to avatar_url

--- database/migrations/2024_12_26_023047_add_avatar_url_to_users_table.php
+++ database/migrations/2024_12_26_023047_add_avatar_url_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->longText('avatar_url');
+            $table->longText('avatar_url')->nullable();
         });
     }
 
