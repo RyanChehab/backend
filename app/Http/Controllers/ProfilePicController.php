7d4d31e072260f2871a0d04f2fa29357c6da00a4
RyanChehab <ryanchehab20@gmail.com>
2024-12-26 02:08:20 +0200

    php <=> created public function upload

--- app/Http/Controllers/ProfilePicController.php
+++ app/Http/Controllers/ProfilePicController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ProfilePicController extends Controller
 {
-    //
+    public function upload(Request $request){
+
+        $request->validate([
+            'profile_pic' => 'required|file|mimes:jpg,jpeg,png|max:2048', // 2MB max
+        ]);
+
+        $file = $request->file('profile_pic');
+        
+    }
 }
