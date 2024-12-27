f5795a8731a932b133459692e4ce7e225362432f
RyanChehab <ryanchehab20@gmail.com>
2024-12-27 15:51:48 +0200

    php <=> saved in db

--- app/Services/BookimageService.php
+++ app/Services/BookimageService.php
@@ -7,4 +7,13 @@ use App\Models\Book;
 
 Class BookimageService{
     protected $route = "https://fan-tales-storage.s3.eu-north-1.amazonaws.com/placeHolder/Book-placeholder(2).png";
+
+    public function addPlaceHolderImg(){
+        $books=Book::whereNull('img_url')->get();
+
+        foreach($books as $book){
+            $book->img_url = $route;
+            $book->save();
+        }
+    }
 }
