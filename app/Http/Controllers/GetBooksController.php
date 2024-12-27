a3dae7a00c0a6b69d3044e115e2fae7f75f59978
RyanChehab <ryanchehab20@gmail.com>
2024-12-27 17:06:10 +0200

    php <=> responded with featured books

--- app/Http/Controllers/GetBooksController.php
+++ app/Http/Controllers/GetBooksController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 
 class GetBooksController extends Controller{
-    public function getFeaturedBooks(){
-        
+    public function getFeaturedBooks():JsonResponse{
+        $featuredBooks = Book::where('featured', 1)->get();
+
+        return response()->json($featuredBooks);
     }
 }
