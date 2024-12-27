8b87e1e057c190a093174b5730066ad38870a4a5
RyanChehab <ryanchehab20@gmail.com>
2024-12-27 15:47:28 +0200

    php <=> used Book model

--- app/Services/BookimageService.php
+++ app/Services/BookimageService.php
@@ -3,7 +3,8 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
+use App\Models\Book;
 
 Class BookimageService{
-    
+    protected $route = "https://fan-tales-storage.s3.eu-north-1.amazonaws.com/placeHolder/Book-placeholder(2).png";
 }
