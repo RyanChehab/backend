a5582b21254dfef1a9561ebb6259c263ea48bc82
RyanChehab <ryanchehab20@gmail.com>
2024-12-27 16:18:50 +0200

    php <=> debbuged

--- app/Services/BookimageService.php
+++ app/Services/BookimageService.php
@@ -2,8 +2,9 @@
 
 namespace App\Services;
 
-use Illuminate\Support\Facades\Http;
 use App\Models\Book;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 
 Class BookimageService{
     protected $route = "https://fan-tales-storage.s3.eu-north-1.amazonaws.com/placeHolder/Book-placeholder(2).png";
@@ -12,8 +13,9 @@ Class BookimageService{
         $books=Book::whereNull('img_url')->get();
 
         foreach($books as $book){
-            $book->img_url = $route;
+            $book->img_url = $this->$route;
             $book->save();
         }
     }
+
 }
