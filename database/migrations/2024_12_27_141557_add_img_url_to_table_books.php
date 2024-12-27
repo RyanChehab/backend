5a7c0656a4912eca732229df8cc57ffbedb0795d
RyanChehab <ryanchehab20@gmail.com>
2024-12-27 14:21:37 +0200

    migrations <=> debugged

--- database/migrations/2024_12_27_141557_add_img_url_to_table_books.php
+++ database/migrations/2024_12_27_141557_add_img_url_to_table_books.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('table_books', function (Blueprint $table) {
+        Schema::table('books', function (Blueprint $table) {
             $table->LongText('img_url')->after('useCount')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('table_books', function (Blueprint $table) {
+        Schema::table('books', function (Blueprint $table) {
             $table->dropColumn('img_url');
         });
     }
