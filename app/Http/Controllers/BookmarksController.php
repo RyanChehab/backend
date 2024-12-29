51cec3f64cc1f79bf91aecc6a2ab97e0c51731b0
RyanChehab <ryanchehab20@gmail.com>
2024-12-29 02:50:58 +0200

    php <=> validated user

--- app/Http/Controllers/BookmarksController.php
+++ app/Http/Controllers/BookmarksController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Bookmark;
+use App\Models\Book;
 
-class BookmarksController extends Controller
-{
-    //
+class BookmarksController extends Controller{
+    public function addBookmark(Request $request)
+    {
+        $request->validate([
+            'bookmarkable_id' => 'required|integer',
+            'bookmarkable_type' => 'required|string',
+        ]);
+
+        $user = JWTAuth::parseToken()->authenticate();
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not authenticated',
+            ], 401);
+        }
+    }
 }
