072bfe03f4c0e5d9b2b35eee52e003c88ba4aa7b
RyanChehab <ryanchehab20@gmail.com>
2025-01-02 21:19:39 +0200

    php <=> authenticated user

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Repository;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class RepositoryController extends Controller{
 
@@ -15,6 +16,15 @@ class RepositoryController extends Controller{
             'img_url' => 'nullable|url',
             'story_url' => 'nullable|url',
         ]);
+
+        $user = JWTAuth::parseToken()->authenticate();
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'message' => 'User not authenticated',
+            ], 401);
+        }
 
         $repository = new Repository();
 
