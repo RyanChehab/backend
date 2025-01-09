2088644d99dc4f2165327b59d4b84fc5be7e8fe8
RyanChehab <ryanchehab20@gmail.com>
2025-01-10 01:01:13 +0200

    php <=> added generateImg function

--- app/Services/OpenAIService.php
+++ app/Services/OpenAIService.php
@@ -9,4 +9,13 @@ class OpenAiService{
     protected $baseUrl;
     protected $apiKey;
 
+    public function __construct(){
+
+        $this->baseUrl = env('OPENAI_BASE_URL', 'https://api.openai.com');
+        $this->apiKey = env('OPENAI_API_KEY');
+    }
+
+    public function generateImg(string $promt, string $size = '400x300'): array{
+        
+    }
 }
