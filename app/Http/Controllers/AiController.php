0482fd43368720abcbd9cd8de8a409c8906a895d
RyanChehab <ryanchehab20@gmail.com>
2025-01-10 01:29:31 +0200

    php <=> returned generated image

--- app/Http/Controllers/AiController.php
+++ app/Http/Controllers/AiController.php
@@ -12,4 +12,25 @@ class AiController extends Controller{
     public function __construct(OpenAiService $openAIService){
         $this->openAaIService = $openAIService;
     }
+
+    public function generateImage(Request $request){
+
+        $validated = $request->validate([
+            'promt' => 'required|string',
+        ]);
+        
+        try{
+            $imageData = $this->openAIService->generateImage($validated['promt']);
+
+            return response()->json([
+                'success' => true,
+                'data' => $imageData,
+            ]);
+        }catch(\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage(),
+            ], 500);
+        }
+    }
 }
