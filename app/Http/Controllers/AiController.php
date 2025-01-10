5985815d5d1059f43c3e693e9a1ae2922d131e63
RyanChehab <ryanchehab20@gmail.com>
2025-01-10 13:55:26 +0200

    php <=> resized image

--- app/Http/Controllers/AiController.php
+++ app/Http/Controllers/AiController.php
@@ -4,28 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\OpenAIService;
+use Illuminate\Support\Facades\Http;
+use Intervention\Image\Facades\Image;
 
 class AiController extends Controller{
 
     protected $openAIService;
 
     public function __construct(OpenAiService $openAIService){
-        $this->openAaIService = $openAIService;
+        $this->openAIService = $openAIService;
     }
 
     public function generateImage(Request $request){
 
         $validated = $request->validate([
-            'promt' => 'required|string',
+            'prompt' => 'required|string',
         ]);
         
         try{
-            $imageData = $this->openAIService->generateImage($validated['promt']);
+            $imageData = $this->openAIService->generateImg($validated['prompt']);
+
+            // accessing the image url generated
+            $imageUrl = $imageData[0];
+            $response = Http::get($imageUrl);
+
+            if ($response->successful()) {
+                // Resize the image
+                $resizedImage = Image::make($response->body())->resize(300, 400);
+    
+                
+            }
 
             return response()->json([
                 'success' => true,
                 'data' => $imageData,
-            ]);
+            ],201);
         }catch(\Exception $e) {
             return response()->json([
                 'success' => false,
