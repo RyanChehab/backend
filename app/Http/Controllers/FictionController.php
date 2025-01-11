8b7786824930dd30d9f15c0dbdf9d7a296dd0b62
RyanChehab <ryanchehab20@gmail.com>
2025-01-11 19:09:08 +0200

    php <=> created storeFiction function

--- app/Http/Controllers/FictionController.php
+++ app/Http/Controllers/FictionController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FictionController extends Controller
-{
-    //
+class FictionController extends Controller{
+    
+    public function storeFiction(Request $request,$id){
+
+        $validated = $request->validate([
+            'content'=> 'required|string',
+        ]);
+
+        
+    }
 }
