4db1f2610573dba2bbc679c6ad14776fb62f530c
RyanChehab <ryanchehab20@gmail.com>
2025-01-13 01:26:54 +0200

    php <=> created controller

---
+++ app/Http/Controllers/GutenBergController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class GutenBergController extends Controller{
+    
+    public function fetchBookContent(Request $request){
+        
+    }
+}
